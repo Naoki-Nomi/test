90fd7e32e9aa6f8c407c2b25f799711e48752745
Naoki-Nomi <n.naoki.116@gmail.com>
2021-02-21 02:10:25 +0000

    [Add]

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '151abb28287123b44185ffb132b702b0993ac9330d494278ad5d9c7f40c2624a105e8138b09fdfdc6640078cc8cec498473fd021292b5f88299b998fa5de66d7'
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  get 'users/show'
+  devise_for :users
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
+  resources :books, except: [:new]
+  resources :users, only: [:show, :index, :edit, :update]
+
+end
